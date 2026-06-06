0372a031c93fffa8e4e1d173f847190a392c22df
bfactoryproject-spec <bfactoryproject-spec@users.noreply.github.com>
2026-06-06 10:53:27 +0000

    deploy: bfactoryproject-spec/slot-survival-game@60a0b01301c523a7b7842a95ffaeb3758fabc09b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1889985633" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1946477413" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
